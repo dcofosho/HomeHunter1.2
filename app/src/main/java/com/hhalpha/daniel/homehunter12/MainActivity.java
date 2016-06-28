abaafe1761825753f80b4e2a13ab82504e658472
dcofosho <dlcohen224@gmail.com>
2016-06-28 07:01:32 -0400

    Got all bundles/prefs in reg activities and main

--- app/src/main/java/com/hhalpha/daniel/homehunter12/MainActivity.java
+++ app/src/main/java/com/hhalpha/daniel/homehunter12/MainActivity.java
@@ -1,7 +1,9 @@
 package com.hhalpha.daniel.homehunter12;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -9,20 +11,31 @@ import android.widget.TextView;
  * Created by Daniel on 6/24/2016.
  */
 public class MainActivity extends Activity {
-    String profileName, loginMethod;
+    String profileName, loginMethod,email, salary;
     Boolean registered;
     TextView textViewMain;
+    SharedPreferences preferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textViewMain=(TextView) findViewById(R.id.textViewMain);
+        preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        try {
+            Bundle bundle = getIntent().getBundleExtra("bundle");
+            email=bundle.getString("email","");
+            salary=bundle.getString("salary","");
+            Log.v("_dan",email+salary);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
         try {
             Bundle bundle = getIntent().getBundleExtra("bundle");
             profileName=bundle.getString("profileName","");
             loginMethod=bundle.getString("loginMethod","");
             registered=bundle.getBoolean("registered",false);
-            textViewMain.setText(profileName+" "+loginMethod + " " +registered.toString());
+            textViewMain.setText(preferences.getAll().toString());
             Log.v("_danMain",bundle.toString());
         }catch(Exception e){
             e.printStackTrace();
--- app/src/main/java/com/hhalpha/daniel/homehunter12/RegisterActivity2.java
+++ app/src/main/java/com/hhalpha/daniel/homehunter12/RegisterActivity2.java
@@ -1,16 +1,59 @@
 package com.hhalpha.daniel.homehunter12;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Daniel on 6/28/2016.
  */
 public class RegisterActivity2 extends Activity {
-
+    EditText editTextSalary,editTextEmail;
+    Integer salary;
+    String email,loginMethod,profileName;
+    Boolean registered;
+    Button reg2Button;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    Bundle bundle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
+        bundle = new Bundle();
+        editTextSalary=(EditText) findViewById(R.id.editTextSalary);
+        editTextEmail=(EditText)findViewById(R.id.editTextEmail);
+        reg2Button=(Button) findViewById(R.id.reg2Button);
+        preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        editor = preferences.edit();
+        try{
+            profileName=preferences.getString("profileName","");
+            loginMethod=preferences.getString("loginMethod","");
+            registered=preferences.getBoolean("registered",false);
+            bundle.putString("profileName",profileName);
+            bundle.putString("loginMethod",loginMethod);
+            bundle.putBoolean("registered",registered);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void submit(View v){
+        salary=Integer.parseInt(editTextSalary.getText().toString());
+        email=editTextEmail.getText().toString();
+        editor.putString("email",email);
+        editor.putInt("salary",salary);
+        editor.apply();
+        Intent i = new Intent(RegisterActivity2.this,MainActivity.class);
+        bundle = new Bundle();
+        bundle.putString("email",email);
+        bundle.putInt("salary",salary);
+        i.putExtra("bundle",bundle);
+        startActivity(i);
     }
 }
